11f0d552b560e85d8fa7092f47f87dc7bae37847
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-09-26 15:11:50 -0500

    added bootstrap

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,19 @@
+// import modules
 import "./form";
 import "./submit";
+
+// import css files
+import '../css/index.css';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'boostrap/dist/css/bootstrap.min.css';
+
+// images
+import Logo from '../images/new-logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+});
